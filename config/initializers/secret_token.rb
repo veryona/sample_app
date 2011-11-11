a3ec9ff2638db34bd83ec3126a23ee7cbf8b77a0
veryona <info@veryona.com>
2011-11-10 22:27:41 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0427996d53f7b61e9161fcd1fb8d2766364aee3e40ea44e3efa551fe5ffed16c871350cb4dd8d0d34050dad0e21c0928adc9304d3c3b36a094c4c5db94231bf5'
